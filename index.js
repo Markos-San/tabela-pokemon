052cd94cdf48fb4f3909ccedc7153ead424df865
Markos-San <133286127+Markos-San@users.noreply.github.com>
2023-05-15 19:23:07 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+const openModalButton = document.querySelector("#modal-add-pokemon");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
 document.getElementById("botao-alterar-tema").addEventListener("click", () => {
   document.body.classList.toggle("modo-escuro");
-  document.body.classList.contains("modo-escuro")
-    ? document.getElementById("imgBtn").setAttribute("src", "./image/moon.png")
-    : document.getElementById("imgBtn").setAttribute("src", "./image/sun.png");
+  if (document.body.classList.contains("modo-escuro")) {
+    document.getElementById("imgBtn").setAttribute("src", "./image/moon.png");
+    modal.style.backgroundColor = "#000";
+  } else {
+    document.getElementById("imgBtn").setAttribute("src", "./image/sun.png");
+    modal.style.backgroundColor = "#fff";
+  }
+});
+
+const toggleModal = () => {
+  [modal, fade].forEach((el) => el.classList.toggle("hide"));
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
 });
