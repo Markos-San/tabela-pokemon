68d8fc7b94b7dc8cea9e7498235f40723a54e71c
Markos-San <133286127+Markos-San@users.noreply.github.com>
2023-05-19 19:12:56 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const inputGifPokemon = document.getElementById("gifPokemon");
 const inputAtribute = document.getElementById("atributo-pokemon");
 const inputAtribute2 = document.getElementById("atributo2-pokemon");
 const textPokemon = document.getElementById("texto-descricao-pokemon");
-const pokeAcc = 10;
+let pokeAcc = 1;
 
 document.getElementById("botao-alterar-tema").addEventListener("click", () => {
   document.body.classList.toggle("modo-escuro");
@@ -49,44 +49,60 @@ const addCardPokemon = (
   inputAtribute2,
   textPokemon
 ) => {
-  const currentPoke = pokeAcc + 1;
+  if (
+    (inputNamePokemon.value !== "" || null) &&
+    inputAtribute.value &&
+    inputAtribute2.value !== "Selecione uma opção..." &&
+    textPokemon.value !== "" &&
+    inputGifPokemon.value !== ""
+  ) {
+    pokeAcc++;
 
-  const ul = document.getElementById("listagem-pokemon");
+    const ul = document.getElementById("listagem-pokemon");
 
-  const newLi = document.createElement("li");
-  newLi.className = "cartao-pokemon modo-claro";
+    const newLi = document.createElement("li");
+    newLi.className = "cartao-pokemon modo-claro";
 
-  const newDiv = document.createElement("div");
-  newDiv.className = "informacoes";
-  newDiv.innerHTML = `<span>${inputNamePokemon.value}</span> <span>"#0${currentPoke}"</span>`;
+    const newDiv = document.createElement("div");
+    newDiv.className = "informacoes";
+    newDiv.innerHTML = `<span>${inputNamePokemon.value}</span> <span>"#${
+      pokeAcc + 9
+    }"</span>`;
 
-  const newImg = document.createElement("img");
-  newImg.className = "gif";
-  newImg.alt = "foto do pokemon";
-  const gif = inputGifPokemon.files[0].name;
+    const newImg = document.createElement("img");
+    newImg.className = "gif";
+    newImg.alt = "foto do pokemon";
+    const gif = inputGifPokemon.files[0].name;
+    newImg.src = `.\\image\\${gif}`;
 
-  newImg.src = `.\\image\\${gif}`;
+    const newUl = document.createElement("ul");
+    newUl.className = "tipos";
 
-  const newUl = document.createElement("ul");
-  newUl.className = "tipos";
+    const tipoLi = document.createElement("li");
+    tipoLi.className = `tipo ${inputAtribute.value}`;
+    tipoLi.innerText = `${inputAtribute.value} `;
+    newUl.append(tipoLi);
 
-  const tipoLi = document.createElement("li");
-  tipoLi.className = `tipo ${inputAtribute.value}`;
-  tipoLi.innerText = `${inputAtribute.value} `;
-  newUl.append(tipoLi);
+    if (inputAtribute2.value !== "none") {
+      const tipoLi2 = document.createElement("li");
+      tipoLi2.className = `tipo ${inputAtribute2.value}`;
+      tipoLi2.innerText = `${inputAtribute2.value} `;
+      newUl.append(tipoLi2);
+    }
 
-  if (inputAtribute2.value !== "none") {
-    const tipoLi2 = document.createElement("li");
-    tipoLi2.className = `tipo ${inputAtribute2.value}`;
-    tipoLi2.innerText = `${inputAtribute2.value} `;
-    newUl.append(tipoLi2);
+    const newP = document.createElement("p");
+    newP.className = "descricao";
+    newP.innerText = `${textPokemon.value}`;
+
+    newLi.append(newDiv, newImg, newUl, newP);
+    ul.appendChild(newLi);
+
+    inputNamePokemon.value = "";
+    inputAtribute.value = "";
+    inputAtribute2.value = "";
+    textPokemon.value = "";
+    inputGifPokemon.value = "";
+  } else {
+    alert("Faltando informações");
   }
-
-  const newP = document.createElement("p");
-  newP.className = "descricao";
-  newP.innerText = `"${textPokemon.value}" `;
-
-  newLi.append(newDiv, newImg, newUl, newP);
-  ul.appendChild(newLi);
-  console.log(gif);
 };
